5ad15775d216a9ab2e06e7455bb56f8d88210074
GYama953 <rultfa2@gmail.com>
2024-11-30 21:04:09 +1000

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,12 @@
+function toggleContent(cellId) {
+    const cell = document.querySelector(`.grid-item:nth-child(${cellId})`);
+    
+    // Если содержимое ещё не создано
+    if (!cell.querySelector('.content')) {
+        const newContent = document.createElement('div');
+        newContent.classList.add('content');
+        newContent.innerHTML = `<h2>Epic</h2><p>промокод</p>`;
+        cell.appendChild(newContent);
+        newContent.style.display = 'flex';  // Показываем содержимое с центровкой
+    }
+}
